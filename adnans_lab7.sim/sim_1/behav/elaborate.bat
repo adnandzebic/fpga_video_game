c609d49f2b3c33ee8e728ebbe4d5d3075567502e
Adnan Dzebic <adnandzebic@gmail.com>
2015-11-17 22:26:29 -0800

    deleted everything

--- adnans_lab7.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.2\\bin
-call %xv_path%/xelab  -wto 648c3faf838c43a49972d6510e71a2f5 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot testbench_behav xil_defaultlib.testbench xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
