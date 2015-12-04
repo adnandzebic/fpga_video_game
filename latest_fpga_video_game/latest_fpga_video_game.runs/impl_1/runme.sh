e1d29a30f69a109b3556d960f048086ece832b24
Adnan Dzebic <adnandzebic@gmail.com>
2015-12-03 22:43:17 -0800

    another commit

--- latest_fpga_video_game/latest_fpga_video_game.runs/impl_1/runme.sh
+++ latest_fpga_video_game/latest_fpga_video_game.runs/impl_1/runme.sh
@@ -6,21 +6,17 @@
 # Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
 # 
 
-echo "This script was generated under a different operating system."
-echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executing this script"
-exit
-
 if [ -z "$PATH" ]; then
-  PATH=C:/Xilinx/Vivado/2015.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2015.2/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2015.2/bin
+  PATH=/opt/Xilinx/Vivado/2015.2/ids_lite/ISE/bin/lin64:/opt/Xilinx/Vivado/2015.2/bin
 else
-  PATH=C:/Xilinx/Vivado/2015.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2015.2/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2015.2/bin:$PATH
+  PATH=/opt/Xilinx/Vivado/2015.2/ids_lite/ISE/bin/lin64:/opt/Xilinx/Vivado/2015.2/bin:$PATH
 fi
 export PATH
 
 if [ -z "$LD_LIBRARY_PATH" ]; then
-  LD_LIBRARY_PATH=
+  LD_LIBRARY_PATH=/opt/Xilinx/Vivado/2015.2/ids_lite/ISE/lib/lin64
 else
-  LD_LIBRARY_PATH=:$LD_LIBRARY_PATH
+  LD_LIBRARY_PATH=/opt/Xilinx/Vivado/2015.2/ids_lite/ISE/lib/lin64:$LD_LIBRARY_PATH
 fi
 export LD_LIBRARY_PATH
 
@@ -41,7 +37,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .write_bitstream.begin.rst
+/bin/touch .init_design.begin.rst
 EAStep vivado -log project.vdi -applog -m64 -messageDb vivado.pb -mode batch -source project.tcl -notrace
 
 
